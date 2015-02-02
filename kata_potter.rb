c22b232369c332cc01252e0d89f161b41fffbd57
chapuzzo <luismax@gmail.com>
2015-02-02 18:53:53 +0100

    two different books get 5% discount

--- kata_potter.rb
+++ kata_potter.rb
@@ -2,6 +2,9 @@
 BOOK_PRICE = 8
 
 def basket books
+  if books.uniq.count == books.count
+    return 15.2 if books.count == 2
+  end
   books.count * BOOK_PRICE
 end
 
--- test_potter.rb
+++ test_potter.rb
@@ -13,4 +13,14 @@ class TestPotter < Test::Unit::TestCase
     assert_equal 8, basket([5])
   end
 
+  def test_to_equal_books_cost_16
+    assert_equal 16, basket([1,1])
+    assert_equal 16, basket([5,5])
+  end
+
+  def test_two_different_books_get_5_percent_discount
+    assert_equal 15.2, basket([1,2])
+    assert_equal 15.2, basket([4,5])
+  end
+
 end
